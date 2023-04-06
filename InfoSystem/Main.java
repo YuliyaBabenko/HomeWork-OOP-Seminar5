1c46992650a9478499afc2a2e1eef7a809e8ab22
Yuliya <ju7iab@gmail.com>
2023-04-06 17:47:32 +0300

    some adds

--- InfoSystem/Main.java
+++ InfoSystem/Main.java
@@ -5,10 +5,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите команду: 1. Добавить сотрудника 2. Удалить сотрудника 3. Редактировать сотрудника");
-
-        Integer in = scanner.nextInt();
         Employer genManager = new Employer("John", "Simpson", "General Manager", 150000);
         Journal<Human> company = new Journal();
 
@@ -18,6 +14,11 @@ public class Main {
         Employee employee2 = new Employee("Jim", "Karter", "Logistician", 12000);
         Employee employee3 = new Employee("Jack", "Smith", "Accountant", 9000);
         Employee employee4 = new Employee("Pamela", "Blazic", "Finance Controller", 16000);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите команду: 1. Добавить сотрудника 2. Удалить сотрудника 3. Редактировать сотрудника");
+
+        Integer in = scanner.nextInt();
 
         if (in == 1) {
             genManager.hireEmployee(employee1);
